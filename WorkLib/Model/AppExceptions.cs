4244504d7a2bdcbb3dbd2db4526f85c5a7469888
Peter Bocej <pbocej@gmail.com>
2021-10-22 15:06:30 +0200

    Create custom app exception

---
+++ WorkLib/Model/AppExceptions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace WorkLib.Model
+{
+
+    /// <summary>
+    /// Base exception for this app.
+    /// </summary>
+    /// <seealso cref="System.Exception" />
+    [Serializable]
+    public class AppException : Exception
+    {
+        #region Constructors
+
+        /// <summary>
+        ///   <para>
+        /// Initializes a new instance of the <see cref="AppException" /> class.
+        /// </para>
+        /// </summary>
+        public AppException() { }
+        /// <summary>Initializes a new instance of the <see cref="AppException" /> class.</summary>
+        /// <param name="message">The message that describes the error.</param>
+        public AppException(string message) : base(message) { }
+        /// <summary>Initializes a new instance of the <see cref="AppException" /> class.</summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="inner">The inner exception.</param>
+        public AppException(string message, Exception inner) : base(message, inner) { }
+        protected AppException(
+          SerializationInfo info,
+          StreamingContext context) : base(info, context) { }
+
+        #endregion
+
+        #region Message manipulations
+
+        /// <summary>Gets the full message and includes all inner exceptions separated by new lisne.</summary>
+        /// <value>The full message.</value>
+        public string FullMessage => GetFullMessage();
+
+        /// <summary>
+        /// Gets the full message.
+        /// </summary>
+        /// <param name="rowSeparator">The row separator (default new line).</param>
+        /// <param name="ex">The exception (default null)</param>
+        /// <returns></returns>
+        public string GetFullMessage(string rowSeparator = "\r\n", Exception ex = null)
+        {
+            if (ex == null)
+                ex = this;
+            var msg = ex.Message;
+            if (ex.InnerException != null)
+                msg += rowSeparator + GetFullMessage(rowSeparator, ex.InnerException);
+        }
+
+        #endregion
+    }
+
+}
---
+++ WorkLib/Repository/WorkRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkLib.Model;
+
+namespace WorkLib.Repository
+{
+    /// <summary>
+    /// Wrapper for data
+    /// </summary>
+    public static class WorkRepository
+    {
+        #region Users
+
+        /// <summary>Gets the user by username.</summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <returns>User</returns>
+        public static User GetUser(string userName)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new AppException($"Error getting user by name: {userName}", ex);
+            }
+            using (var context = new MyWorkEntities())
+            {
+                return
+                    (from user in context.Users
+                    where user.UserName == userName
+                    select user).FirstOrDefault<User>();
+            }
+        }
+
+        #endregion
+
+        #region Projects
+        #endregion
+
+        #region Work
+
+        #endregion
+    }
+}
